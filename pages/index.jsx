abf8fef5d765ea9f09ee609a1fe3deba14688b2d
Mt.Qwerty <mumo1126@gmail.com>
2023-06-16 14:38:04 +0900

    update main側でも変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,7 +37,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>半農半IT/ハートランド/Web3/ポスト資本主義</p>
+        <p>半農半IT/ハートランド/Web3/ポスト資本主義/とかいろいろ</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
